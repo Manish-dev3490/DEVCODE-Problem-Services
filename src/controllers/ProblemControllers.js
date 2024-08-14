72d8b827c971a6d54380ea6db2899a04b267e554
manish-dev3490 <kmanish87064@gmail.com>
2024-08-14 20:06:44 +0530

    Implemented 501 status code

--- src/controllers/ProblemControllers.js
+++ src/controllers/ProblemControllers.js
@@ -1,22 +1,32 @@
 function addProblem(req, res) {
-
+    return res.status(501).json({
+        msg:'Not implemented'
+    });
 }
 
 function getProblem(req, res) {
-
+    return res.status(501).json({
+        msg:'Not implemented'
+    });
 }
 
 function getProblems(req, res) {
-
+    return res.status(501).json({
+        msg:'Not implemented'
+    });
 }
 
 
 function deleteProblem(req, res) {
-
+    return res.status(501).json({
+        msg:'Not implemented'
+    });
 }
 
 function updateProblem(req, res) {
-
+    return res.status(501).json({
+        msg:'Not implemented'
+    });
 }
 function pingcontrollercheck(req, res) {
     res.json({ msg: "ping controller check is working " });
