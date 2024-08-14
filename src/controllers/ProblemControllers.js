b31b7f3073a66b31094c53f5c2d020bb5c5eb648
manish-dev3490 <kmanish87064@gmail.com>
2024-08-14 20:15:52 +0530

    implemented http-status-codes package

--- src/controllers/ProblemControllers.js
+++ src/controllers/ProblemControllers.js
@@ -1,30 +1,32 @@
+const {StatusCodes}=require('http-status-codes');
+
 function addProblem(req, res) {
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         msg:'Not implemented'
     });
 }
 
 function getProblem(req, res) {
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         msg:'Not implemented'
     });
 }
 
 function getProblems(req, res) {
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         msg:'Not implemented'
     });
 }
 
 
 function deleteProblem(req, res) {
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         msg:'Not implemented'
     });
 }
 
 function updateProblem(req, res) {
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         msg:'Not implemented'
     });
 }
